22bc61fea146683a3d323dc052c16d6d9a0c173c
Augustus Sroka <rokaawoo@gmail.com>
2025-01-08 18:51:00 -0500

    add current word length display debug in word_print function

--- assignments/1-C-Refresher/submission/stringfun-complete.c
+++ assignments/1-C-Refresher/submission/stringfun-complete.c
@@ -97,6 +97,7 @@ void word_print(char *buff, int str_len){
         } else {
             printf("%c", buff[i]);
             current_word_len++;
+            printf("(%d)", current_word_len);
         }
     }
     printf("\n");
