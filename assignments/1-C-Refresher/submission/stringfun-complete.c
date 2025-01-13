b17a980a0fa4cb4752c7ed9442c95620d1a4e8ca
Augustus Sroka <rokaawoo@gmail.com>
2025-01-13 15:15:52 -0500

    fix: correct variable declaration syntax for replace_word and replace_with in main function

--- assignments/1-C-Refresher/submission/stringfun-complete.c
+++ assignments/1-C-Refresher/submission/stringfun-complete.c
@@ -115,7 +115,8 @@ void word_replace(char *buff, int str_len, char *replace_word, char *replace_wit
 
 int main(int argc, char *argv[]){
 
-    char *replace_word, *replace_with;
+    char *replace_word
+    char *replace_with;
 
     char *buff;             //placehoder for the internal buffer
     char *input_string;     //holds the string provided by the user on cmd line
