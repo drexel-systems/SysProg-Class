c6bfb8ffd3227f908ae6fa93066418e45857be58
Jose Guzman Rodriguez <jrg355@drexel.edu>
2025-02-11 20:27:45 -0500

    print output

--- assignments/3-ShellP1/starter/dsh_cli.c
+++ assignments/3-ShellP1/starter/dsh_cli.c
@@ -49,29 +49,36 @@ int main()
     char cmd_buff[SH_CMD_MAX];
     int rc = 0;
     command_list_t clist;
-    
+
+    memset(cmd_buff, 0, SH_CMD_MAX);
     while(1){
         printf("%s", SH_PROMPT);
         if (fgets(cmd_buff, ARG_MAX, stdin) == NULL){
             printf("\n");
             break;
-        }    
+        }
+            //remove the trailing \n from cmd_buff
         cmd_buff[strcspn(cmd_buff,"\n")] = '\0';
 
-        if (strcmp(cmd_buff, EXIT_CMD) == 0){
+        //IMPLEMENT THE REST OF THE REQUIREMENTS
+        if (strcmp(cmd_buff, EXIT_CMD) == 0) {
             exit(EXIT_SUCCESS);
-        } else {
-            rc = build_cmd_list(cmd_buff, &clist);
-            if (rc == OK) {
-                printf(CMD_OK_HEADER, clist.num);
-            } else if (rc == WARN_NO_CMDS) {
-                printf(CMD_WARN_NO_CMD);
-            } else if (rc == ERR_TOO_MANY_COMMANDS) {
-                printf(CMD_ERR_PIPE_LIMIT);
+        }
+
+        rc = build_cmd_list(cmd_buff, &clist);
+        if (rc == OK) {
+            for (int i = 0; i < clist.num; i++) {
+                printf("<%d> %s", i+1, clist.commands[i].exe);
+                if (clist.commands[i].args[0] != '\0') {
+                    printf("[%s]", clist.commands[i].args);
+                }
+                printf("\n");
             }
+
+            printf(CMD_OK_HEADER, clist.num);
         }
     }
 
-    return 0;
+    printf(M_NOT_IMPL);
+    exit(EXIT_NOT_IMPL);
 }
-    
