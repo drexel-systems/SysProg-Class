b177e4dfdcb5c77e4fec509be080128dc8a0d79f
Jose Guzman Rodriguez <jrg355@drexel.edu>
2025-02-27 21:00:55 -0500

    copy previous week's code

--- assignments/5-ShellP3/starter/dshlib.c
+++ assignments/5-ShellP3/starter/dshlib.c
@@ -52,8 +52,160 @@
  *  Standard Library Functions You Might Want To Consider Using (assignment 2+)
  *      fork(), execvp(), exit(), chdir()
  */
-int exec_local_cmd_loop()
-{
-   
-    return OK;
-}
+ int exec_local_cmd_loop()
+ {
+     char *cmd_input;
+     int rc = 0;
+     cmd_buff_t cmd_buff;
+     Built_In_Cmds bi_cmd;
+     
+     // IMPLEMENT MAIN LOOP
+     cmd_input = malloc(SH_CMD_MAX);
+     if (cmd_input == NULL) {
+         perror("malloc:");
+         return ERR_MEMORY;
+     }
+     memset(cmd_input, 0, SH_CMD_MAX);
+     
+     while(1){
+         clear_cmd_buff(&cmd_buff);
+         printf("%s", SH_PROMPT);
+         if (fgets(cmd_input, ARG_MAX, stdin) == NULL){
+             printf("\n");
+             break;
+         }
+         //remove the trailing \n from cmd_buff
+         cmd_input[strcspn(cmd_input,"\n")] = '\0';
+         
+         // IMPLEMENT parsing input to cmd_buff_t *cmd_buff
+         rc = build_cmd_buff(cmd_input, &cmd_buff);
+         if (rc == WARN_NO_CMDS) {
+             printf(CMD_WARN_NO_CMD);
+             continue;
+         } else if (rc == ERR_TOO_MANY_COMMANDS) {
+             printf(CMD_ERR_PIPE_LIMIT, CMD_MAX);
+             continue;
+         }
+         
+         // IMPLEMENT if built-in command, execute builtin logic for exit, cd (extra credit: dragon)
+         // the cd command should chdir to the provided directory; if no directory is provided, do nothing
+         bi_cmd = exec_built_in_cmd(&cmd_buff);
+         if (bi_cmd == BI_CMD_EXIT) {
+             return OK;
+         } else if (bi_cmd == BI_EXECUTED) {
+         } else if (bi_cmd == BI_RC) {
+         } else if (bi_cmd == BI_NOT_BI) {
+             // IMPLEMENT if not built-in command, fork/exec as an external command
+             rc = exec_cmd(&cmd_buff);
+             if (rc == ERR_EXEC_CMD) {
+                 printf("Error executing command %s\n", cmd_buff.argv[0]);
+             }
+         }
+     }
+     
+     // for example, if the user input is "ls -l", you would fork/exec the command "ls" with the arg "-l"
+     
+     free(cmd_input);
+     return OK;
+ }
+ 
+ int build_cmd_buff(char *cmd_line, cmd_buff_t *cmd_buff)
+ {
+     char *p;
+     int i;
+     int start_token = 0;
+     int start_quote = 0;
+ 
+     if (cmd_line[0] == '\0') {
+         return WARN_NO_CMDS;
+     }
+     
+     for (i = 0, p = cmd_line; *p != '\0'; p++) {
+         if (*p == '"') {
+             if (start_quote) {
+                 start_quote = 0;
+                 start_token = 0;
+             } else {
+                 start_quote = 1;
+             }
+             *p = '\0';
+         } else if (*p == SPACE_CHAR && !start_quote) {
+             start_token = 0;
+             *p = '\0';
+         } else if (!start_token) {
+             start_token = 1;
+             cmd_buff->argv[i] = p;
+             i++;
+         }
+     }
+     cmd_buff->argc = i;
+     cmd_buff->_cmd_buffer = cmd_line;
+     
+     return OK;
+ }
+ 
+ Built_In_Cmds exec_built_in_cmd(cmd_buff_t *cmd)
+ {
+     Built_In_Cmds bi_cmd;
+     bi_cmd = match_command(cmd->argv[0]);
+     int rc;
+     
+     switch (bi_cmd) {
+     case BI_CMD_EXIT:
+         return BI_CMD_EXIT;
+     case BI_CMD_CD:
+         rc = chdir(cmd->argv[1]);
+         if (rc != 0) {
+             return BI_RC;
+         }
+         return BI_EXECUTED;
+     case BI_NOT_BI:
+         return BI_NOT_BI;
+     default:
+         return BI_RC;
+     }
+ }
+ 
+ 
+ Built_In_Cmds match_command(const char *input)
+ {
+     if (strcmp(input, EXIT_CMD) == 0) {
+         return BI_CMD_EXIT;
+     } else if (strcmp(input, "cd") == 0) {
+         return BI_CMD_CD;
+     } else {
+         return BI_NOT_BI;
+     }
+ }
+ 
+ int exec_cmd(cmd_buff_t *cmd)
+ {
+     pid_t f_result, c_result;
+     int rc = 0;
+ 
+     f_result = fork();
+     if (f_result < 0) {
+         return ERR_EXEC_CMD;
+     }
+ 
+     if (f_result == 0) {
+         rc = execvp(cmd->argv[0], cmd->argv);
+         if (rc < 0) {
+             exit(EXIT_FAILURE);
+         }
+     } else {
+         wait(&c_result);
+         rc = WEXITSTATUS(c_result);
+         if (rc != OK) {
+             return ERR_EXEC_CMD;
+         }
+     }
+ 
+     return OK;
+ }
+ 
+ int clear_cmd_buff(cmd_buff_t *cmd_buff)
+ {
+     memset(cmd_buff, 0, sizeof(cmd_buff_t));
+     return OK;
+ }
