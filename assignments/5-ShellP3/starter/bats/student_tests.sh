75134878f454037d8f751435ef7900754ce926da
Jose Guzman Rodriguez <jrg355@drexel.edu>
2025-03-01 12:47:33 -0500

    Copy tests from previous week

--- assignments/5-ShellP3/starter/bats/student_tests.sh
+++ assignments/5-ShellP3/starter/bats/student_tests.sh
@@ -12,3 +12,68 @@ EOF
     # Assertions
     [ "$status" -eq 0 ]
 }
+
+# Copy assignment tests from previous week to make sure nothing breaks
+@test "Which which ... which?" {
+    run "./dsh" <<EOF                
+which which
+EOF
+
+    # Strip all whitespace (spaces, tabs, newlines) from the output
+    stripped_output=$(echo "$output" | tr -d '[:space:]')
+
+    # Expected output with all whitespace removed for easier matching
+    expected_output="/usr/bin/whichdsh3>dsh3>cmdloopreturned0"
+
+    # These echo commands will help with debugging and will only print
+    #if the test fails
+    echo "Captured stdout:" 
+    echo "Output: $output"
+    echo "Exit Status: $status"
+    echo "${stripped_output} -> ${expected_output}"
+
+    # Check exact match
+    [ "$stripped_output" = "$expected_output" ]
+}
+
+@test "It handles quoted spaces" {
+    run "./dsh" <<EOF                
+   echo " hello     world     " 
+EOF
+
+    # Strip all whitespace (spaces, tabs, newlines) from the output
+    stripped_output=$(echo "$output" | tr -d '\t\n\r\f\v')
+
+    # Expected output with all whitespace removed for easier matching
+    expected_output=" hello     world     dsh3> dsh3> cmd loop returned 0"
+
+    # These echo commands will help with debugging and will only print
+    #if the test fails
+    echo "Captured stdout:" 
+    echo "Output: $output"
+    echo "Exit Status: $status"
+    echo "${stripped_output} -> ${expected_output}"
+
+    # Check exact match
+    [ "$stripped_output" = "$expected_output" ]
+}
+
+# Copy student tests from previous week to make sure nothing breaks
+
+@test "Incorrect command given" {
+    run ./dsh <<EOF
+pwf
+EOF
+
+    stripped_output=$(echo "$output" | tr -d '[:space:]')
+
+    expected_output="dsh3>dsh3>Errorexecutingcommandpwfdsh3>cmdloopreturned0"
+
+    echo "Captured stdout:" 
+    echo "Output: $output"
+    echo "Exit Status: $status"
+    echo "${stripped_output} -> ${expected_output}"
+    
+    [ "$stripped_output" = "$expected_output" ]
+
+}
