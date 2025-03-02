197de470d66c55a5713547069c49fdd326f91003
Jose Guzman Rodriguez <jrg355@drexel.edu>
2025-03-01 19:29:27 -0500

    reformat

--- assignments/5-ShellP3/starter/dshlib.c
+++ assignments/5-ShellP3/starter/dshlib.c
@@ -1,64 +1,64 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
 #include <ctype.h>
-#include <stdbool.h>
-#include <unistd.h>
 #include <fcntl.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/wait.h>
+#include <unistd.h>
 
 #include "dshlib.h"
 
 /*
-* Implement your exec_local_cmd_loop function by building a loop that prompts the 
-* user for input.  Use the SH_PROMPT constant from dshlib.h and then
-* use fgets to accept user input.
-* 
-*      while(1){
-*        printf("%s", SH_PROMPT);
-*        if (fgets(cmd_buff, ARG_MAX, stdin) == NULL){
-*           printf("\n");
-*           break;
-*        }
-*        //remove the trailing \n from cmd_buff
-*        cmd_buff[strcspn(cmd_buff,"\n")] = '\0';
-* 
-*        //IMPLEMENT THE REST OF THE REQUIREMENTS
-*      }
-* 
-*   Also, use the constants in the dshlib.h in this code.  
-*      SH_CMD_MAX              maximum buffer size for user input
-*      EXIT_CMD                constant that terminates the dsh program
-*      SH_PROMPT               the shell prompt
-*      OK                      the command was parsed properly
-*      WARN_NO_CMDS            the user command was empty
-*      ERR_TOO_MANY_COMMANDS   too many pipes used
-*      ERR_MEMORY              dynamic memory management failure
-* 
-*   errors returned
-*      OK                     No error
-*      ERR_MEMORY             Dynamic memory management failure
-*      WARN_NO_CMDS           No commands parsed
-*      ERR_TOO_MANY_COMMANDS  too many pipes used
-*   
-*   console messages
-*      CMD_WARN_NO_CMD        print on WARN_NO_CMDS
-*      CMD_ERR_PIPE_LIMIT     print on ERR_TOO_MANY_COMMANDS
-*      CMD_ERR_EXECUTE        print on execution failure of external command
-* 
-*  Standard Library Functions You Might Want To Consider Using (assignment 1+)
-*      malloc(), free(), strlen(), fgets(), strcspn(), printf()
-* 
-*  Standard Library Functions You Might Want To Consider Using (assignment 2+)
-*      fork(), execvp(), exit(), chdir()
-*/
+ * Implement your exec_local_cmd_loop function by building a loop that prompts
+ * the user for input.  Use the SH_PROMPT constant from dshlib.h and then use
+ * fgets to accept user input.
+ *
+ *      while(1){
+ *        printf("%s", SH_PROMPT);
+ *        if (fgets(cmd_buff, ARG_MAX, stdin) == NULL){
+ *           printf("\n");
+ *           break;
+ *        }
+ *        //remove the trailing \n from cmd_buff
+ *        cmd_buff[strcspn(cmd_buff,"\n")] = '\0';
+ *
+ *        //IMPLEMENT THE REST OF THE REQUIREMENTS
+ *      }
+ *
+ *   Also, use the constants in the dshlib.h in this code.
+ *      SH_CMD_MAX              maximum buffer size for user input
+ *      EXIT_CMD                constant that terminates the dsh program
+ *      SH_PROMPT               the shell prompt
+ *      OK                      the command was parsed properly
+ *      WARN_NO_CMDS            the user command was empty
+ *      ERR_TOO_MANY_COMMANDS   too many pipes used
+ *      ERR_MEMORY              dynamic memory management failure
+ *
+ *   errors returned
+ *      OK                     No error
+ *      ERR_MEMORY             Dynamic memory management failure
+ *      WARN_NO_CMDS           No commands parsed
+ *      ERR_TOO_MANY_COMMANDS  too many pipes used
+ *
+ *   console messages
+ *      CMD_WARN_NO_CMD        print on WARN_NO_CMDS
+ *      CMD_ERR_PIPE_LIMIT     print on ERR_TOO_MANY_COMMANDS
+ *      CMD_ERR_EXECUTE        print on execution failure of external command
+ *
+ *  Standard Library Functions You Might Want To Consider Using (assignment 1+)
+ *      malloc(), free(), strlen(), fgets(), strcspn(), printf()
+ *
+ *  Standard Library Functions You Might Want To Consider Using (assignment 2+)
+ *      fork(), execvp(), exit(), chdir()
+ */
 int exec_local_cmd_loop()
 {
     char *cmd_input;
     int rc = 0;
     cmd_buff_t cmd_buff;
     Built_In_Cmds bi_cmd;
-    
+
     // IMPLEMENT MAIN LOOP
     cmd_input = malloc(SH_CMD_MAX);
     if (cmd_input == NULL) {
@@ -66,17 +66,17 @@ int exec_local_cmd_loop()
         return ERR_MEMORY;
     }
     memset(cmd_input, 0, SH_CMD_MAX);
-    
-    while(1){
+
+    while (1) {
         clear_cmd_buff(&cmd_buff);
         printf("%s", SH_PROMPT);
-        if (fgets(cmd_input, ARG_MAX, stdin) == NULL){
+        if (fgets(cmd_input, ARG_MAX, stdin) == NULL) {
             printf("\n");
             break;
         }
-        //remove the trailing \n from cmd_buff
-        cmd_input[strcspn(cmd_input,"\n")] = '\0';
-        
+        // remove the trailing \n from cmd_buff
+        cmd_input[strcspn(cmd_input, "\n")] = '\0';
+
         // IMPLEMENT parsing input to cmd_buff_t *cmd_buff
         rc = build_cmd_buff(cmd_input, &cmd_buff);
         if (rc == WARN_NO_CMDS) {
@@ -86,24 +86,27 @@ int exec_local_cmd_loop()
             printf(CMD_ERR_PIPE_LIMIT, CMD_MAX);
             continue;
         }
-        
-        // IMPLEMENT if built-in command, execute builtin logic for exit, cd (extra credit: dragon)
-        // the cd command should chdir to the provided directory; if no directory is provided, do nothing
+
+        // IMPLEMENT if built-in command, execute builtin logic for exit, cd
+        // (extra credit: dragon) the cd command should chdir to the provided
+        // directory; if no directory is provided, do nothing
         bi_cmd = exec_built_in_cmd(&cmd_buff);
         if (bi_cmd == BI_CMD_EXIT) {
             return OK;
         } else if (bi_cmd == BI_EXECUTED) {
         } else if (bi_cmd == BI_NOT_BI) {
-            // IMPLEMENT if not built-in command, fork/exec as an external command
+            // IMPLEMENT if not built-in command, fork/exec as an external
+            // command
             rc = exec_cmd(&cmd_buff);
             if (rc == ERR_EXEC_CMD) {
                 printf("Error executing command %s\n", cmd_buff.argv[0]);
             }
         }
     }
-    
-    // for example, if the user input is "ls -l", you would fork/exec the command "ls" with the arg "-l"
-    
+
+    // for example, if the user input is "ls -l", you would fork/exec the
+    // command "ls" with the arg "-l"
+
     free(cmd_input);
     return OK;
 }
@@ -114,11 +117,11 @@ int build_cmd_buff(char *cmd_line, cmd_buff_t *cmd_buff)
     int i;
     int start_token = 0;
     int start_quote = 0;
-    
+
     if (cmd_line[0] == '\0') {
         return WARN_NO_CMDS;
     }
-    
+
     for (i = 0, p = cmd_line; *p != '\0'; p++) {
         if (*p == '"') {
             if (start_quote) {
@@ -139,7 +142,7 @@ int build_cmd_buff(char *cmd_line, cmd_buff_t *cmd_buff)
     }
     cmd_buff->argc = i;
     cmd_buff->_cmd_buffer = cmd_line;
-    
+
     return OK;
 }
 
@@ -148,26 +151,25 @@ Built_In_Cmds exec_built_in_cmd(cmd_buff_t *cmd)
     Built_In_Cmds bi_cmd;
     bi_cmd = match_command(cmd->argv[0]);
     int rc;
-    
+
     switch (bi_cmd) {
-        case BI_CMD_EXIT:
+    case BI_CMD_EXIT:
         return BI_CMD_EXIT;
-        case BI_CMD_CD:
+    case BI_CMD_CD:
         rc = chdir(cmd->argv[1]);
         if (rc != 0) {
             return BI_CMD_CD;
         }
         return BI_EXECUTED;
-        case BI_NOT_BI:
+    case BI_NOT_BI:
         return BI_NOT_BI;
-        case BI_CMD_DRAGON:
+    case BI_CMD_DRAGON:
         return BI_CMD_DRAGON;
-        default:
+    default:
         fprintf(stderr, "Fatal error");
         exit(EXIT_FAILURE);
     }
 }
-
 
 Built_In_Cmds match_command(const char *input)
 {
@@ -186,12 +188,12 @@ int exec_cmd(cmd_buff_t *cmd)
 {
     pid_t f_result, c_result;
     int rc = 0;
-    
+
     f_result = fork();
     if (f_result < 0) {
         return ERR_EXEC_CMD;
     }
-    
+
     if (f_result == 0) {
         rc = execvp(cmd->argv[0], cmd->argv);
         if (rc < 0) {
@@ -204,7 +206,7 @@ int exec_cmd(cmd_buff_t *cmd)
             return ERR_EXEC_CMD;
         }
     }
-    
+
     return OK;
 }
 
@@ -236,7 +238,8 @@ int clear_cmd_buff(cmd_buff_t *cmd_buff)
 //     for (i = 0; i < clist->num; i++) {
 //         exe = strtok(raw_cmds[i], " ");
 
-//         if (strlen(exe) >= EXE_MAX || strlen(raw_cmds[i]) - strlen(exe) >= SH_CMD_MAX) {
+//         if (strlen(exe) >= EXE_MAX || strlen(raw_cmds[i]) - strlen(exe) >=
+//         SH_CMD_MAX) {
 //             return ERR_CMD_OR_ARGS_TOO_BIG;
 //         }
 //         strcpy(clist->commands[i].exe, exe);
