4462ffe33921513f39e08d07c09b86603a76be82
Jose Guzman Rodriguez <jrg355@drexel.edu>
2025-03-02 22:17:25 -0500

    add new test

--- assignments/5-ShellP3/starter/bats/student_tests.sh
+++ assignments/5-ShellP3/starter/bats/student_tests.sh
@@ -77,3 +77,20 @@ EOF
     [ "$stripped_output" = "$expected_output" ]
 
 }
+
+@test "Pipe character in quote" {
+    run ./dsh <<EOF
+echo "this pipe | is in a quote"
+EOF
+
+    stripped_output=$(echo "$output" | tr -d '[:space:]')
+
+    expected_output="thispipe|isinaquotedsh3>dsh3>cmdloopreturned0"
+
+    echo "Captured stdout:"
+    echo "Output: $output"
+    echo "Exit Status: $status"
+    echo "${stripped_output} -> ${expected_output}"
+
+    [ "$stripped_output" = "$expected_output" ]
+}
